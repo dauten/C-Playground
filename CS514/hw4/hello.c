129942dc8474cc9e96789a9754cf068d55cbf2fb
Dale Auten <dauten96@gmail.com>
2019-05-06 23:29:00 -0500

    basis of FUSE

---
+++ CS514/hw4/hello.c
@@ -0,0 +1,132 @@
+/*
+  FUSE: Filesystem in Userspace
+  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+
+  This program can be distributed under the terms of the GNU GPL.
+  See the file COPYING.
+
+  gcc -Wall `pkg-config fuse --cflags --libs` hello.c -o hello
+*/
+
+#define FUSE_USE_VERSION 30
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+static const char *hello_str = "Hello World!\n";
+static const char *hello_path = "/hello";
+static const char *hello_path2 = "/hello_dir/hello";
+static const char *hello_dir = "/hello_dir";
+
+static int hello_getattr(const char *path, struct stat *stbuf)
+{
+	int res = 0;
+
+	memset(stbuf, 0, sizeof(struct stat));
+	if (strcmp(path, "/") == 0) {
+		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 2;
+	} else if (strcmp(path, hello_path) == 0) {
+		stbuf->st_mode = S_IFREG | 0444;
+		stbuf->st_nlink = 1;
+		stbuf->st_size = strlen(path);
+	}
+	else if (strcmp(path, hello_path2) == 0) {
+		stbuf->st_mode = S_IFREG | 0444;
+		stbuf->st_nlink = 1;
+		stbuf->st_size = strlen("Content of this one file");
+	}
+	else if (strcmp(path, "/hello_path") == 0) {
+		stbuf->st_mode = S_IFREG | 0744;
+		stbuf->st_nlink = 1;
+		stbuf->st_size = strlen(hello_str);
+	}
+	else if (strcmp(path, hello_dir) == 0) {
+		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 1;
+	} else
+		res = -ENOENT;
+
+	return res;
+}
+
+static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+			 off_t offset, struct fuse_file_info *fi)
+{
+	(void) offset;
+	(void) fi;
+
+	if (strcmp(path, "/") != 0){
+		if(strcmp(path, hello_dir) != 0){
+			return -ENOENT;
+		}
+		else{
+			filler(buf, ".", NULL, 0);
+			filler(buf, "..", NULL, 0);
+			filler(buf, hello_path + 1, NULL, 0);
+			return 0;
+		}
+
+
+		return -ENOENT;
+
+	}
+
+	filler(buf, ".", NULL, 0);
+	filler(buf, "..", NULL, 0);
+	filler(buf, hello_path + 1, NULL, 0);
+	filler(buf, "/hello_path" + 1 , NULL, 0);
+	filler(buf, hello_dir + 1, NULL, 0);
+
+	return 0;
+}
+
+static int hello_open(const char *path, struct fuse_file_info *fi)
+{
+	if (strcmp(path, hello_path2) != 0 && strcmp(path, hello_path) != 0 && strcmp(path, "/hello_path") != 0&& strcmp(path, hello_dir) != 0)
+		return -ENOENT;
+
+	if ((fi->flags & 3) != O_RDONLY)
+		return -EACCES;
+
+	return 0;
+}
+
+static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+		      struct fuse_file_info *fi)
+{
+	size_t len;
+	(void) fi;
+	if(strcmp(path, hello_path2) != 0 && strcmp(path, hello_path) != 0 && strcmp(path, "/hello_path") != 0 && strcmp(path, hello_dir) != 0)
+		return -ENOENT;
+
+	len = strlen("Content of this one file");
+	if (offset < len) {
+		//if (offset + size > len)
+		//	size = len - offset;
+		memcpy(buf, "Content of this one file" + offset, len);
+	} else
+		size = 0;
+
+	return size;
+}
+
+//static int hello_opendir(const char* path, off_t offset, struct fust_file_info* fi){
+
+//}
+
+static struct fuse_operations hello_oper = {
+	.getattr	= hello_getattr,
+	.readdir	= hello_readdir,
+	.open		= hello_open,
+	.read		= hello_read,
+//	.readdir  = hello_opendir,
+};
+
+int main(int argc, char *argv[])
+{
+	return fuse_main(argc, argv, &hello_oper, NULL);
+}
